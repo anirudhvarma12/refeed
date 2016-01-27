e632103627db0e0118276377321615d893a97394
Anirudh Varma <varma93@hotmail.com>
2016-01-27 17:02:26 +0530

    Add path to get RSS

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask import render_template
 
-from models import db
+from models import db,FeedItem
+import rss
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -12,11 +13,22 @@ db.init_app(app)
 @app.before_first_request
 def create_database():
     print("attempting database creation")
+    # db.create_all()
+    # item1 = FeedItem("http://stackoverflow.com/questions/13213048/urllib2-http-error-429")
+    # item2 = FeedItem("http://flask-sqlalchemy.pocoo.org/2.1/quickstart/")
+    # db.session.add(item1)
+    # db.session.add(item2)
+    # db.session.commit()
 
 
 @app.route("/")
 def hello():
     return render_template("index.html")
+
+
+@app.route("/feed")
+def get_feed():
+    return rss.get_rss()
 
 if __name__ == "__main__":
     app.run()
--- rss.py
+++ rss.py
@@ -1,12 +1,30 @@
 from bs4 import BeautifulSoup
 from models import FeedItem
+import datetime
+import PyRSS2Gen
+import urllib
 
 
 def get_feed_items():
     return FeedItem.query.all()
 
-# def get_rss():
-# 	all_feeds = get_feed_items()
-# 	size = len(all_feeds)
-# 	for i in range(size):
-# 		soup = BeautifulSoup()
+
+def get_rss():
+    all_feeds = get_feed_items()
+    size = len(all_feeds)
+    rss = PyRSS2Gen.RSS2(
+        title="Devs and Hackers Feed", lastBuildDate=datetime.datetime.now(),
+        link="foo", description="ffee")
+    for i in range(size):
+        rss.items.append(convert(all_feeds[i]))
+
+    return rss.to_xml()
+
+
+def convert(item):
+    print("Parsing item " + item.url)
+    request = urllib.request.Request(
+        item.url, headers={"User-Agent": "Mozila/5.0"})
+    client = urllib.request.urlopen(request)
+    soup = BeautifulSoup(client.read(), "html.parser")
+    return PyRSS2Gen.RSSItem(title=soup.title.string, link=item.url, description="")
